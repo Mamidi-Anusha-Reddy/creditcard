e187c748022acae54a6f6b8f2be536ada44b75ad
Mamidi-Anusha-Reddy <mamidianusha333@gmail.com>
2025-09-03 15:16:12 +0530

    Update ApplicationController.java

--- src/creditcard/controller/ApplicationController.java
+++ src/creditcard/controller/ApplicationController.java
@@ -1,56 +1,38 @@
 package creditcard.controller;
 
 
-import com.example.creditcard.model.Application;
-import com.example.creditcard.model.Document;
-import com.example.creditcard.repository.ApplicationRepository;
-import com.example.creditcard.repository.DocumentRepository;
+package com.scb.creditcardapplication.controller;
+
+import com.scb.creditcardapplication.model.Application;
+import com.scb.creditcardapplication.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ApplicationController {
 
-    private final ApplicationRepository applicationRepository;
-    private final DocumentRepository documentRepository;
+    @Autowired
+    private ApplicationRepository applicationRepository;
 
-    public ApplicationController(ApplicationRepository applicationRepository, DocumentRepository documentRepository) {
-        this.applicationRepository = applicationRepository;
-        this.documentRepository = documentRepository;
-    }
-
-    // Save application
+    // POST: Save application + documents
     @PostMapping
     public Application createApplication(@RequestBody Application application) {
         return applicationRepository.save(application);
     }
 
-    // Get all applications
+    // GET: fetch all applications
     @GetMapping
     public List<Application> getAllApplications() {
         return applicationRepository.findAll();
     }
 
-    // Get application by ID
+    // GET: fetch application by id
     @GetMapping("/{id}")
     public Application getApplicationById(@PathVariable Long id) {
         return applicationRepository.findById(id).orElseThrow();
     }
-
-    // Save documents
-    @PostMapping("/{appId}/documents")
-    public Document uploadDocuments(@PathVariable Long appId, @RequestBody Document document) {
-        Application application = applicationRepository.findById(appId).orElseThrow();
-        document.setApplication(application);
-        return documentRepository.save(document);
-    }
-
-    // Get documents by application ID
-    @GetMapping("/{appId}/documents")
-    public Document getDocumentsByApplicationId(@PathVariable Long appId) {
-        return documentRepository.findByApplicationId(appId);
-    }
 }
-
