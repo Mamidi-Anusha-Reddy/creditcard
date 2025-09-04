544cee8e99965d421d0ad44e2b74815ed2240a9f
Mamidi-Anusha-Reddy <mamidianusha333@gmail.com>
2025-09-04 10:50:58 +0530

    Update ApplicationRepository.java

--- src/creditcard/repository/ApplicationRepository.java
+++ src/creditcard/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
-package com.scb.creditcardapplication.repository;
+package com.scb.creditcardorigination.applicationFormFeature.repository;
 
-import com.scb.creditcardapplication.model.Application;
+import com.scb.creditcardorigination.applicationFormFeature.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplicationRepository extends JpaRepository<Application, Long> {}
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
