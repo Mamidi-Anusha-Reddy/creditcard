b4ba35cfa3b42b4f6c595c74b656a56cf20ac4d5
Mamidi-Anusha-Reddy <mamidianusha333@gmail.com>
2025-09-05 12:28:19 +0530

    Update ApplicationRepository.java

--- src/creditcard/repository/ApplicationRepository.java
+++ src/creditcard/repository/ApplicationRepository.java
@@ -2,6 +2,14 @@ package com.scb.creditcardorigination.applicationFormFeature.repository;
 
 import com.scb.creditcardorigination.applicationFormFeature.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    // Custom query methods can be added here if needed
+    List<Application> findByEmail(String email);
+    List<Application> findByCreditCardType(String creditCardType);
+    List<Application> findByProfileType(String profileType);
 }
